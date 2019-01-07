2a2bca5c6ebf45758950eda22d0c63f5d77be9c6
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-01-07 12:06:59 +0100

    Updated and optimized toRoman.js

--- src/toRoman.js
+++ src/toRoman.js
@@ -1,17 +1,21 @@
-function toRoman (num) {
-  if (isNaN(num)) {
-    return NaN;
-  } else {
-    var digits = String(+num).split(''),
-    key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
-           "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
-           "","I","II","III","IV","V","VI","VII","VIII","IX"],
-    roman = '', i = 3;
-    while (i--) {
-      roman = ( key[+digits.pop() + (i * 10)] || '' ) + roman;
+function isRoman (num) {
+  var result = '';
+  var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+
+  decimal.forEach( function(number, index) {
+    // looping over every element of the arrays
+    while ( (num % number) < num ) {
+      // keep trying the same number until we need to move to a smaller one
+
+      result += roman[index];
+      // add the matching roman number to our result string
+
+      num -= number;
+      // remove the decimal value of the roman number from our number
     }
-    return Array(+digits.join('') + 1).join('M') + roman;
-  }
+  });
+  return result;
 }
 
-module.exports = toRoman;
+console.log(isRoman(95))
