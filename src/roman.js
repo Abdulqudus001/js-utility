8c7bb9d3795f1b3999fbcf48ecbc73b8682730d4
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-01-07 12:57:22 +0100

    Added from and to roman implementation

---
+++ src/roman.js
@@ -0,0 +1,41 @@
+var decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+var roman = ["M", "CM","D","CD","C", "XC", "L", "XL", "X","IX","V","IV","I"];
+
+function toRoman (num) {
+  var result = '';
+  decimal.forEach( function(number, index) {
+    // looping over every element of the arrays
+    while ( (num % number) < num ) {
+      // keep trying the same number until we need to move to a smaller one
+
+      result += roman[index];
+      // add the matching roman number to our result string
+
+      num -= number;
+      // remove the decimal value of the roman number from our number
+    }
+  });
+  return result;
+}
+
+function fromRoman (str) {
+  var result = 0;
+  decimal.forEach( function(number, index) {
+    // looping over every element of the arrays
+    while ( str.indexOf(roman[index]) === 0 ) {
+      // Get first roman number from string
+
+      result += number;
+      // add the matching decimal number to our result
+
+      str = str.replace(roman[index], '');
+      // remove the roman number from the string
+    }
+  });
+  return result;
+}
+
+module.exports = {
+  toRoman: toRoman,
+  fromRoman: fromRoman
+}
