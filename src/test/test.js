9720782e22469f718d2f4001acd656a700acb9e0
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-07 21:21:56 +0100

    Added tests for roman conversions

--- src/test/test.js
+++ src/test/test.js
@@ -1,5 +1,8 @@
 const assert = require('chai').assert;
+const expect = require('chai').expect;
 const palindrome = require('../../index').isPalindrome;
+const toRoman = require('../../index').toRoman;
+const fromRoman = require('../../index').fromRoman;
 
 describe('Check palindrome', () => {
   it('should return true', () => {
@@ -8,5 +11,14 @@ describe('Check palindrome', () => {
 
   it('should return false', () => {
     assert.equal(palindrome('myname'), false);
+  });
+});
+
+describe('Convert to Roman', () => {
+  it('57 should return LVII', () => {
+    assert.equal(toRoman(57), 'LVII');
+  });
+  it('XCVII should return 97', () => {
+    expect(fromRoman('XCVII')).to.equal(97);
   });
 });
