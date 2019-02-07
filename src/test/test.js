f7232885fcf92f9118455e84adfd8e88f696882c
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-07 12:00:18 +0100

    Added test for palindrome

--- src/test/test.js
+++ src/test/test.js
@@ -2,7 +2,11 @@ const assert = require('chai').assert;
 const palindrome = require('../../index').isPalindrome;
 
 describe('Check palindrome', () => {
-  it('should return the palindrome', () => {
+  it('should return true', () => {
     assert.equal(palindrome('nurses run'), true);
-  })
+  });
+
+  it('should return false', () => {
+    assert.equal(palindrome('myname'), false);
+  });
 });
