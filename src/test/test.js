d938e737884a85ba4c8e8eda54861cad7a5364dc
Abubakre Abdulqudus <abdulqudusabubakre@gmail.com>
2019-02-07 21:27:19 +0100

    Seperated tests for toRoman and fromRoman

--- src/test/test.js
+++ src/test/test.js
@@ -3,13 +3,15 @@ const expect = require('chai').expect;
 const palindrome = require('../../index').isPalindrome;
 const toRoman = require('../../index').toRoman;
 const fromRoman = require('../../index').fromRoman;
+const isAlphaNumeric = require('../../index').isAlphaNumeric;
+const toAlphaNumeric = require('../../index').toAlphaNumeric;
 
 describe('Check palindrome', () => {
-  it('should return true', () => {
+  it('"nurses run" should return true', () => {
     assert.equal(palindrome('nurses run'), true);
   });
 
-  it('should return false', () => {
+  it('"myname" should return false', () => {
     assert.equal(palindrome('myname'), false);
   });
 });
@@ -18,7 +20,22 @@ describe('Convert to Roman', () => {
   it('57 should return LVII', () => {
     assert.equal(toRoman(57), 'LVII');
   });
+  it('97 should return XCVII', () => {
+    expect(toRoman(97)).to.equal('XCVII');
+  });
+});
+
+describe('Convert from roman', () => {
   it('XCVII should return 97', () => {
     expect(fromRoman('XCVII')).to.equal(97);
   });
+
+  it('CMVII should return 907', () => {
+    expect(fromRoman('CMVII')).to.equal(907);
+  });
 });
+
+
+describe('Check if string is alphanumeric', () => {
+
+})
